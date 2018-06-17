d6e987a9dccf32adc2220c044ca79e2bed7b7aae
Max Sinyakov <max@sinyakov.com>
2018-06-17 18:15:46 +0300

    fix

--- src/TicketList.js
+++ src/TicketList.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React from 'react';
 import Ticket from './Ticket';
 
-export default (props) => {
-    const {tickets, isLoading, error, filters: {currency, stops}} = props;
-    if(isLoading){
-        return(<div>isLoading</div>);
-    }
-    if(error){
-        return(<div>Error</div>);
-    }
-    const filteredTickets = stops.length === 0
-        ? tickets
-        : tickets.filter(ticket => stops.includes(ticket.stops));
-    return (<div>{filteredTickets.map(ticket => <Ticket ticket={ticket} currency={currency}/>)}</div>);
-}
+export default props => {
+  const {
+    tickets,
+    isLoading,
+    error,
+    filters: { currency, stops }
+  } = props;
+
+  if (isLoading) {
+    return <div>isLoading</div>;
+  }
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  const filteredTickets = stops.length
+    ? tickets.filter(ticket => stops.includes(ticket.stops))
+    : tickets;
+
+  return filteredTickets.map(ticket => (
+    <Ticket ticket={ticket} currency={currency} />
+  ));
+};
